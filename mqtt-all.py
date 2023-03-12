6abd2500e38c04948d9c8265d91b4e73b93c39fc
Francis Theodore Catte <perturbed.old.cat@gmail.com>
2023-03-12 18:56:31 -0400

    Major functionality upgrade (see list of changes below).
    Added:
    - ability to toggle the Enviro's display off via an argument (--no-oled; default: enabled).
    - ability to set the temperature sensor compensation factor via an argument (--comp_factor, default: 2.25).
    - read and provide microphone amplitude (low, medium, high, and all frequencies)
    - experimentally enable and read ADC#3 for a GUVA-S12SD UV-B sensor to provide an UV Index value.
    
    -Improved:
    - average temperature sensor across 5 readings to reduce variance, and use a float for temperature instead of an int (for more resolution).
    - now print **all** arguments at runtime

--- mqtt-all.py
+++ mqtt-all.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
 """
 Run mqtt broker on localhost: sudo apt-get install mosquitto mosquitto-clients
 
-Example run: python3 mqtt-all.py --broker 192.168.1.164 --topic enviro
+Example run: python3 mqtt-all.py --broker 192.168.1.164 --topic enviro --username xxx --password xxxx
 """
-#!/usr/bin/env python3
 
 import argparse
 import ST7735
 import time
+import ssl
+import RPi.GPIO as GPIO
 from bme280 import BME280
 from pms5003 import PMS5003, ReadTimeoutError, SerialTimeoutError
 from enviroplus import gas
+from enviroplus.noise import Noise
+from statistics import mean
 
 try:
     # Transitional fix for breaking change in LTR559
@@ -38,6 +42,12 @@ DEFAULT_MQTT_BROKER_IP = "localhost"
 DEFAULT_MQTT_BROKER_PORT = 1883
 DEFAULT_MQTT_TOPIC = "enviroplus"
 DEFAULT_READ_INTERVAL = 5
+DEFAULT_TLS_MODE = False
+DEFAULT_USERNAME = None
+DEFAULT_PASSWORD = None
+DEFAULT_OLED = True
+DEFAULT_COMP_FACTOR = 2.25
+
 
 # mqtt callbacks
 def on_connect(client, userdata, flags, rc):
@@ -52,14 +62,19 @@ def on_publish(client, userdata, mid):
 
 
 # Read values from BME280 and return as dict
-def read_bme280(bme280):
-    # Compensation factor for temperature
-    comp_factor = 2.25
+def read_bme280(bme280, comp_factor):
     values = {}
-    cpu_temp = get_cpu_temperature()
-    raw_temp = bme280.get_temperature()  # float
-    comp_temp = raw_temp - ((cpu_temp - raw_temp) / comp_factor)
-    values["temperature"] = int(comp_temp)
+    # Average temperature readings for more accuracy
+    average = []
+    for x in range(5):
+        cpu_temp = get_cpu_temperature()
+        raw_temp = bme280.get_temperature() # float
+        temp_temp = raw_temp - ((cpu_temp - raw_temp) / comp_factor)
+        average.append(temp_temp)
+        time.sleep(0.1) # Give a little more time between readings
+    # Then round to one decimal point precision
+    comp_temp = round(mean(average), 1)
+    values["temperature"] = comp_temp
     values["pressure"] = round(
         int(bme280.get_pressure() * 100), -1
     )  # round to nearest 10
@@ -69,6 +84,7 @@ def read_bme280(bme280):
     values["reduced"] = int(data.reducing / 1000)
     values["nh3"] = int(data.nh3 / 1000)
     values["lux"] = int(ltr559.get_lux())
+    values["uv_index"] = int(get_uv(data))
     return values
 
 
@@ -95,7 +111,33 @@ def get_cpu_temperature():
         ["vcgencmd", "measure_temp"], stdout=PIPE, universal_newlines=True
     )
     output, _error = process.communicate()
-    return float(output[output.index("=") + 1 : output.rindex("'")])
+    return float(output[output.index("=") + 1:output.rindex("'")])
+
+
+# Get UV Index
+def get_uv(data):
+    """
+    Read ADC #3 input, then divide by default Enviro+ gain (6.148), and multiply by 10.
+    This will give a rough UV Index value based on the mV output of the GUVA-S12SD.
+    You may need to change the UV Index calculation for your analog UV sensor.
+    """
+    uv_index = round((int(data.adc) / 6.148) * 10)
+    if uv_index > 11:
+        # Ignore any index value over 11
+        return '11'
+    else:
+        return uv_index
+
+
+# Get Noise Profile and return as dict
+def get_noise_profile(noise):
+    values = {}
+    low, mid, high, amp = noise.get_noise_profile()
+    values["noise_low"] = round(low * 128, 1)
+    values["noise_mid"] = round(mid * 128, 1)
+    values["noise_high"] = round(high * 128, 1)
+    values["noise_total"] = round(amp * 64, 1)
+    return values
 
 
 # Get Raspberry Pi serial number to use as ID
@@ -165,6 +207,36 @@ def main():
         type=int,
         help="the read interval in seconds",
     )
+    parser.add_argument(
+        "--tls",
+        default=DEFAULT_TLS_MODE,
+        action='store_true',
+        help="enable TLS"
+    )
+    parser.add_argument(
+        "--username",
+        default=DEFAULT_USERNAME,
+        type=str,
+        help="mqtt username"
+    )
+    parser.add_argument(
+        "--password",
+        default=DEFAULT_PASSWORD,
+        type=str,
+        help="mqtt password"
+    )
+    parser.add_argument(
+        "--oled",
+        default=DEFAULT_OLED,
+        action=argparse.BooleanOptionalAction,
+        help="enable or disable display"
+    )
+    parser.add_argument(
+        "--comp_factor",
+        default=DEFAULT_COMP_FACTOR,
+        type=float,
+        help="temperature compensation factor"
+    )
     args = parser.parse_args()
 
     # Raspberry Pi ID
@@ -178,6 +250,12 @@ def main():
     client_id: {device_id}
     port: {args.port}
     topic: {args.topic}
+    interval: {args.interval}
+    tls: {args.tls}
+    username: {args.username}
+    password: {args.password}
+    oled: {args.oled}
+    comp_factor: {args.comp_factor}
 
     Press Ctrl+C to exit!
 
@@ -185,8 +263,17 @@ def main():
     )
 
     mqtt_client = mqtt.Client(client_id=device_id)
+    if args.username and args.password:
+        mqtt_client.username_pw_set(args.username, args.password)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_publish = on_publish
+
+    if args.tls is True:
+        mqtt_client.tls_set(tls_version=ssl.PROTOCOL_TLSv1_2)
+
+    if args.username is not None:
+        mqtt_client.username_pw_set(args.username, password=args.password)
+
     mqtt_client.connect(args.broker, port=args.port)
 
     bus = SMBus(1)
@@ -194,19 +281,37 @@ def main():
     # Create BME280 instance
     bme280 = BME280(i2c_dev=bus)
 
+    # Setup ADC#3
+    gas.setup()
+    gas.enable_adc(True)
+
+    # Create Noise instance
+    # The measurement duration also (indirectly) sets our publish interval
+    noise = Noise(duration=args.interval)
+
     # Create LCD instance
     disp = ST7735.ST7735(
         port=0, cs=1, dc=9, backlight=12, rotation=270, spi_speed_hz=10000000
     )
 
-    # Initialize display
-    disp.begin()
+    if args.oled is True:
+        # Initialize display
+        disp.begin()
+    elif args.oled is False:
+        # Initialize display (even though it's being disabled below)
+        disp.begin()
+        # Blank display
+        disp.command(0x28)
+        # Turn off the display backlight (this function is broken in the ST7735 library)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.output(12, GPIO.LOW)
 
     # Try to create PMS5003 instance
     HAS_PMS = False
     try:
         pms5003 = PMS5003()
-        pm_values = pms5003.read()
+        _ = pms5003.read()
         HAS_PMS = True
         print("PMS5003 sensor is connected")
     except SerialTimeoutError:
@@ -221,15 +326,18 @@ def main():
     mqtt_client.loop_start()
     while True:
         try:
-            values = read_bme280(bme280)
+            values = read_bme280(bme280, args.comp_factor)
+            noise_values = get_noise_profile(noise)
+            values.update(noise_values)
             if HAS_PMS:
                 pms_values = read_pms5003(pms5003)
                 values.update(pms_values)
             values["serial"] = device_serial_number
             print(values)
-            mqtt_client.publish(args.topic, json.dumps(values))
-            display_status(disp, args.broker)
-            time.sleep(args.interval)
+            mqtt_client.publish(args.topic, json.dumps(values), retain=True)
+            if args.oled is True:
+                display_status(disp, args.broker)
+            #time.sleep(args.interval)
         except Exception as e:
             print(e)
 
